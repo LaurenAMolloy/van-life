9f5e1999b2d7561657528b1ef12ac4ad387f6eb2
lauren_m <laurenamolloy@gmail.com>
2025-05-13 11:52:52 +0300

    Added styles to vans list pages

--- src/pages/Host/ListedVans/index.jsx
+++ src/pages/Host/ListedVans/index.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 export default function ListedVans() {
+    const [hostVans, setHostVans] = useState([]);
+
+    //useEffect to grab the host vans data
+    useEffect(() => {
+        fetch(`/api/host/vans`)
+        .then(res => res.json())
+        .then(data => setHostVans(data.vans))
+    },[]);
+
+    //map over vans
+    const listedVans = hostVans.map(van =>
+     <div key={van.id} className="listed-van">
+        <img src={van.imageUrl} alt={van.name}></img>
+        <div className="listed-van-name">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+        </div>
+     </div>
+    )
+
   return (
-    <h1>Listed Vans Page</h1>
+    <>
+    <h1 className="list-title">Your Listed Vans</h1>
+    <div className="listed-van-container">
+    {hostVans.length > 0 ? listedVans : <h2>Loading...</h2>}
+    </div>
+    </>
   )
 }
